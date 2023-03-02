620b88899db83a18bed07c4fb28efbd8b3b6c480
Steve <stevedsimkins@gmail.com>
2023-03-02 11:52:43 -0500

    added partytown

--- astro.config.ts
+++ astro.config.ts
@@ -7,30 +7,31 @@ import prefetch from "@astrojs/prefetch";
 import nordTheme from "./nord.json";
 
 // https://astro.build/config
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
 export default defineConfig({
-	site: "https://stevedsimkins.dev",
-	markdown: {
-		shikiConfig: {
-			theme: nordTheme,
-			wrap: true,
-		},
-	},
-	integrations: [
-		mdx({}),
-		tailwind({
-			config: {
-				applyBaseStyles: false,
-			},
-		}),
-		image({
-			serviceEntryPoint: "@astrojs/image/sharp",
-		}),
-		sitemap(),
-		prefetch(),
-	],
-	vite: {
-		optimizeDeps: {
-			exclude: ["@resvg/resvg-js"],
-		},
-	},
+  site: "https://stevedsimkins.dev",
+  markdown: {
+    shikiConfig: {
+      theme: nordTheme,
+      wrap: true
+    }
+  },
+  integrations: [mdx({}), tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), sitemap(), prefetch(), partytown({
+      config: {
+        forward: ["dataLayer.push"]
+      }
+    })],
+  vite: {
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"]
+    }
+  }
 });
