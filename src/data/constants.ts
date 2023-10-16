14bf70fae4eee4831641eb68963b45fa9a8b1172
Steve <stevedsimkins@gmail.com>
2023-10-15 21:19:27 -0400

    altered links

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,28 +1,27 @@
 export const MENU_LINKS = [
-	{
-		title: "Home",
-		path: "/",
-	},
-	{
-		title: "About",
-		path: "/about",
-	},
-	{
-		title: "Blog",
-		path: "/posts",
-	},
-	{
-		title: "Videos",
-		path: "/videos",
-	},
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+  {
+    title: "Blog",
+    path: "/posts",
+  },
+  {
+    title: "Videos",
+    path: "/videos",
+  },
 ];
 
 // ! Remember to add your own socials
 export const SOCIAL_LINKS = {
-	github: "https://github.com/stevedylandev",
-	twitter: "https://twitter.com/stevedylandev",
-	threads: "https://threads.net/stevedylandev",
-	youtube: "https://youtube.com/@stevedylandev",
-	twitch: "https://twitch.tv/stevedylandev",
-	medium: "https://medium.com/@stevedylandev",
+  github: "https://github.com/stevedylandev",
+  twitter: "https://twitter.com/stevedylandev",
+  youtube: "https://youtube.com/@stevedylandev",
+  ethereum: "https://rainbow.me/stevedylandev.eth",
+  medium: "https://medium.com/@stevedylandev",
 };
