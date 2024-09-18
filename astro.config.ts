8793ccfee4ae256bb1bebd9b16db520826d00eee
Steve <stevedsimkins@gmail.com>
2024-09-17 20:22:44 -0400

    updated to latest version of astro

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,8 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
-
-// https://astro.build/config
-import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
@@ -25,22 +21,13 @@ export default defineConfig({
 				applyBaseStyles: false,
 			},
 		}),
-		image({
-			serviceEntryPoint: "@astrojs/image/sharp",
-		}),
 		sitemap(),
 		prefetch(),
 	],
 	vite: {
-		optimizeDeps: {
-			exclude: ["@resvg/resvg-js"],
-		},
 		define: {
 			"process.env.NODE_ENV": `'${process.env.NODE_ENV}'`,
 		},
 	},
 	output: "static",
-	experimental: {
-		viewTransitions: true,
-	},
 });
