4aef500b6835e777789016c9216c96cb6de2ff5d
Steve <stevedsimkins@gmail.com>
2024-09-20 08:10:36 -0400

    WIP

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,4 +3,9 @@ import { createClient } from "@supabase/supabase-js";
 export const supabase = createClient(
 	import.meta.env.PUBLIC_SUPABASE_URL,
 	import.meta.env.PUBLIC_SUPABASE_ANON_KEY,
+	{
+		auth: {
+			flowType: "pkce",
+		},
+	},
 );
