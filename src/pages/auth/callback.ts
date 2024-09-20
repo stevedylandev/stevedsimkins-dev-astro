5046a412f3b1fdbcf3a64249795f1f6967a13b4f
Steve <stevedsimkins@gmail.com>
2024-09-20 06:32:47 -0400

    WIP

--- src/pages/auth/callback.ts
+++ src/pages/auth/callback.ts
@@ -8,8 +8,8 @@ export const GET: APIRoute = async ({ request, redirect }) => {
 
 	if (code) {
 		const supabase = createServerClient(
-			"https://rbhamoqatlwbdxlydlcw.supabase.com",
-			"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJiaGFtb3FhdGx3YmR4bHlkbGN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY3MzkwNDEsImV4cCI6MjA0MjMxNTA0MX0.prA8jmaA5d-mK-LVyBWCvsWb6g2kV6yHQFoLYxRNgW8",
+			import.meta.env.PUBLIC_SUPABASE_URL,
+			import.meta.env.PUBLIC_SUPABASE_ANON_KEY,
 			{
 				cookies: {
 					getAll() {
