72bd8461a40004508a0527daf33642de724ba340
Steve <stevedsimkins@gmail.com>
2025-03-13 13:54:38 -0400

    fix: Updated rss.xml #5

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,18 +2,18 @@ import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import siteMeta from "@/site-config";
 
-export const get = async () => {
-	const posts = await getCollection("post");
+export async function GET() {
+  const posts = await getCollection("post");
 
-	return rss({
-		title: siteMeta.title,
-		description: siteMeta.description,
-		site: import.meta.env.SITE,
-		items: posts.map((post) => ({
-			title: post.data.title,
-			description: post.data.description,
-			pubDate: post.data.publishDate,
-			link: post.slug,
-		})),
-	});
+  return rss({
+    title: siteMeta.title,
+    description: siteMeta.description,
+    site: "https://stevedylan.dev",
+    items: posts.map((post) => ({
+      title: post.data.title,
+      description: post.data.description,
+      pubDate: post.data.publishDate,
+      link: post.slug,
+    })),
+  });
 };
